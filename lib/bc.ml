af6fc17378d3c397d1cb3ce35757c68360fcdb0b
Minsung <minsung@ccs.neu.edu>
2023-09-28 21:16:14 -0400

    add functionality

--- lib/bc.ml
+++ lib/bc.ml
@@ -3,22 +3,10 @@
   as shown in Section 4 of the paper
 *)
 
-open Rsdd
-(* open Core_grammar *)
+(* open Rsdd
+open Core_grammar
 open Core
-
-type eu = rsdd_expected_utility
-type weight_fn = (int64 * eu * eu) list
-
-type cf = 
-{
-  builder : rsdd_bdd_builder;
-  unn : rsdd_bdd_ptr;
-  acc : rsdd_bdd_ptr;
-  fn : weight_fn ;
-  rw : Int64.Set.t
-}
-
+open Rsdd_abstractions *)
 
 
 
--- lib/rsdd_abstractions.ml
+++ lib/rsdd_abstractions.ml
@@ -4,12 +4,129 @@
 *)
 
 open Rsdd
-open Core_grammar
 open Core
+(* open Core_grammar
 open Hashtbl
-open Set
+open Set *)
 
-type eu = rsdd_expected_utility
-type weight_fn = Std.Heap.create compare
+(*
+  We make a data structure containing:
+  builder : the bdd builder we pass around
+  unn : the unnormalized formula
+  acc : the accepting formula
+  fn : the weight fn
+  rw : the set of reward variables to track 
+*)
+
+type eu = (float * float)
+type weight_fn = (int64 * (eu * eu)) list
 type rw_set = Int64.Set.t
 
+let mk_eu (a :float) (b : float) : eu = (a,b)
+
+type cf = 
+{
+  builder : rsdd_bdd_builder;
+  unn : rsdd_bdd_ptr;
+  acc : rsdd_bdd_ptr;
+  fn : weight_fn;
+  rw : rw_set
+}
+
+let rec lookup (lbl : int64) (l : weight_fn) : bool =
+  match l with 
+  | [] -> false
+  | (lbl', _) :: xs -> if Int64.equal lbl lbl' then true else lookup lbl xs
+
+let rec merge_weight_fns (l1 : weight_fn) (l2 : weight_fn) : weight_fn =
+  match l1 with
+  | [] -> l2
+  | x :: xs -> 
+    match l2 with 
+    | [] -> l1 
+    | _ -> if lookup (fst x) l2 then merge_weight_fns xs l2 else merge_weight_fns xs (x::l2)
+
+
+(*
+  Some constants to make the actual implementation look nicer.
+*)
+
+let t (bdd : rsdd_bdd_builder) : cf = 
+  {
+    builder = bdd;
+    unn = bdd_true bdd;
+    acc = bdd_true bdd;
+    fn = [];
+    rw = Int64.Set.empty
+  }
+
+let f (bdd : rsdd_bdd_builder) : cf = 
+  {
+    builder = bdd;
+    unn = bdd_false bdd;
+    acc = bdd_true bdd;
+    fn = [];
+    rw = Int64.Set.empty
+  }
+
+let mk_newvar (bdd : rsdd_bdd_builder) (wt : eu * eu) : cf = 
+  let (lbl, ptr) = bdd_new_var bdd true in 
+  {
+    builder = bdd;
+    unn = ptr;
+    acc = bdd_true bdd;
+    fn = [(lbl, wt)];
+    rw = Set.add Int64.Set.empty lbl
+  }
+
+(* Helper functions and their infix operations. *)
+
+let cf_not (x : cf) : cf = 
+  {
+    builder = x.builder;
+    unn = bdd_negate x.builder x.unn;
+    acc = x.acc ; fn = x.fn ; rw = x.rw
+  }
+
+let cf_and (a : cf) (b: cf): cf = 
+  {
+    builder = b.builder;
+    unn = bdd_and b.builder a.unn b.unn;
+    acc = bdd_and b.builder a.acc b.acc;
+    fn = merge_weight_fns a.fn b.fn;
+    rw = Set.union a.rw b.rw
+  }
+
+let cf_or (a : cf) (b: cf): cf = 
+  {
+    builder = b.builder;
+    unn = bdd_or b.builder a.unn b.unn;
+    acc = bdd_or b.builder a.acc b.acc;
+    fn = merge_weight_fns a.fn b.fn;
+    rw = Set.union a.rw b.rw
+  }
+
+let cf_ite (a : cf) (b : cf) (c : cf): cf =
+  {
+    builder = c.builder;
+    unn = bdd_ite c.builder a.unn b.unn c.unn;
+    acc = bdd_ite c.builder a.unn b.acc c.acc;
+    fn = merge_weight_fns (merge_weight_fns a.fn b.fn) c.fn;
+    rw = Set.union (Set.union a.rw b.rw) c.rw
+  }
+
+let (&&) a b = cf_and a b
+let (||) a b = cf_or a b
+let (!) a = cf_not a
+
+(* The ExactlyOne constraint *)
+let make_unit x = let p : eu * eu = ((1.0, 0.0), (1.0, 0.0)) in (x,p)
+
+let exactly_one (b : rsdd_bdd_builder) (l : int64 list) : cf = 
+  {
+    builder = b;
+    unn = bdd_exactlyone b l;
+    acc = bdd_true b;
+    fn = List.map l ~f:make_unit;
+    rw = Int64.Set.empty
+  }
