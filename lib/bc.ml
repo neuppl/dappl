794e423b70ea3186d80f1bc8c5d9de19525d39f7
Minsung Cho <minsung@ccs.neu.edu>
2024-03-22 17:53:06 -0400

    asdf

--- lib/bc.ml
+++ lib/bc.ml
@@ -7,125 +7,67 @@ open Rsdd
 open Core_grammar
 open Core
 open Rsdd_abstractions
-(* open Typechecker *)
+open Typechecker
 open Hashtbl
-open Pp
+(* Pp for debugging purposes only *)
+(* open Pp *)
 
-(* We maintain an association list of strings and VarLabels 
-  to enforce exhaustive patternmatch in ChooseWith. *)
-
-let dlist : ((string * rsdd_var_label) list) ref = ref [];;
-let lookup : string -> rsdd_var_label = fun x ->
-  let o = List.Assoc.find !dlist ~equal:String.equal x in
-  match o with 
-  | None -> failwith "unbound decision error??"
-  | Some ptr -> (*let _ = Printf.printf "found!\n" in*) ptr
-
-(*
-  We make a dictionary of identifiers --> BDDPtrs to avoid allocating
-  multiple newvars for the same flip (which will give us the wrong answer).
+(* 
+  Architecture:
+  ---------------------------------
+  dappl program 
+      |
+    (bc)
+      |
+      v
+  tuple of 
+  unnormalized formula bexpr
+  accepting formula bexpr
+  String -> Wt Map.t of variables
+      |
+    (translate)
+      |
+      v
+  bddptrs
+      |
+    (infer)
+      |
+      v 
+  MEU
 *)
 
-let dictionary = create (module String);;
+type bexpr = 
+| TT | FF 
+| Id          of string
+| And         of bexpr * bexpr
+| Or          of bexpr * bexpr
+| Xor         of bexpr * bexpr
+| Not         of bexpr
+| ExactlyOne  of bexpr list
 
-let rec bc (dappl : expr) (bdd : rsdd_bdd_builder) : cf = 
-  match dappl with 
-  | True                ->  t bdd
-  | False               ->  f bdd
-  | And (x,y)           ->  let e1 = bc x bdd in 
-                            let e2 = bc y bdd in 
-                            cf_and bdd e1 e2
-  | Or (x,y)            ->  let e1 = bc x bdd in 
-                            let e2 = bc y bdd in 
-                            cf_or bdd e1 e2
-  | Xor (x,y)           ->  let e1 = bc (Not (And (x,y))) bdd in 
-                            let e2 = bc (Or(x,y)) bdd in 
-                            cf_and bdd e1 e2
-  | Not x               ->  cf_not bdd (bc x bdd)
-  | Ite (x,y,z)         ->  let ex = bc x bdd in 
-                            let ey = bc y bdd in
-                            let ez = bc z bdd in
-                            cf_ite bdd ex ey ez
-  | ChooseWith (d, l)   ->  (* compile the decision *) 
-                            let ed = bc d bdd in
-                            (* l becomes a list of (ptr to name, pattern) *) 
-                            let ir = List.map l ~f:(fun (a,b) -> (bdd_var bdd (lookup a) true, bc b bdd)) in
-                            let ir2 = List.map ir ~f:(fun (a,b) -> cf_and bdd (mk_from_ptr bdd a) b) in
-                            (* which is then folded *)
-                            let l = List.fold ir2 ~init:(f bdd) ~f:(cf_or bdd) in
-                            cf_and bdd ed l
-                            (* for (a,b) in l_of_behaviors *)
-  | Flip n              ->  mk_newvar_prob bdd n
-  | Reward k            ->  mk_newvar_rew bdd k
-  | Decision l          ->  let (e, l) = mk_newvar_dec bdd l in 
-                            let _ : unit = dlist := List.append !dlist l in 
-                            e
-  | Bind (s, e, e')     ->  let cf_e = bc e bdd in
-                            let _ : unit= Hashtbl.add_exn dictionary ~key:s ~data:cf_e in 
-                            bc e' bdd
-  | Observe (e,e')      ->  let e = bc e bdd in 
-                            let e' = bc e' bdd in
-                            { unn = e'.unn;
-                              acc = bdd_and bdd e.unn e'.acc;
-                              fn = merge_weight_fns e.fn e'.fn ; rw = e'.rw
-                            }
-  | Sequence(e, e')     ->  bc (And(e, e')) bdd
-  | Ident x             ->  let ox = Hashtbl.find dictionary x in
-                            (match ox with 
-                            | None -> failwith "unbound variable!"
-                            | Some x' -> x')
-  | _         -> failwith "fuck"
+type wt = (float * float) * (float * float)
 
-(*Debug fn*)
-let rt_newest_lbl bdd = let (lbl, _) = bdd_new_var bdd true in 
-                        Printf.printf "asdfasdf %n\n" (Int64.to_int_exn lbl)
+let prob_to_expect : float -> wt = fun pr ->
+  ((1.0 -. pr, 0.0), (pr, 0.0))
+let rw_to_expect : float -> wt = fun rw ->
+  ((1.0 , 0.0), (1.0, rw))
 
-let infer (prog : program) : float * float = 
-  let program = prog.body in
-  (* typecheck *)
-  (* is_well_typed program ; *)
-  Printf.printf "typechecked!\n" ;
-  (* compile first *)
-  let new_bdd = mk_bdd_builder_default_order 0L in
-  let cf = bc program new_bdd in 
-  (* then remember decisions, store as varlabel list *)
-  let decisions = List.map !dlist ~f:(fun (_,b) -> b) in
-  (* Then we make the WMC param. *)
-  (* The below line may be an unnecessary step, but I didn't do rigorous enough testing to make sure *)
-  let sorted_weight_fn = List.sort cf.fn ~compare:(fun x y -> Int64.compare (fst x) (fst y))  in 
-  let wmc_map = List.map sorted_weight_fn ~f:(fun x -> snd x) in
-  let (lbl, _) = bdd_new_var new_bdd true in 
-  let _ : unit = if not(Int64.to_int_exn lbl = (List.length wmc_map)) 
-          then failwith "live variable-weight mismatch error: do you have any dead code?" 
-          else () in  
-  let wmcparam = new_wmc_params_eu wmc_map in
-  (* Finally the actual MEU *)
-  let unn_and_acc = bdd_and new_bdd cf.unn cf.acc in
-  let (meu, _) = bdd_meu unn_and_acc cf.acc decisions lbl wmcparam in 
-  extract meu
+type tbl = (string, wt, String.comparator_witness) Map.t
 
-type tbl = Wt of (float * float) * (float * float) | Expr of expr
+let empty_tbl : tbl = Map.empty (module String)
 
-let prob_to_expect : float -> tbl = fun pr ->
-  Wt ((1.0 -. pr, 0.0), (pr, 0.0))
-let rw_to_expect : float -> tbl = fun rw ->
-  Wt ((1.0 , 0.0), (1.0, rw))
+let rec bc : expr -> bexpr * bexpr * tbl = fun e ->
+  let (unn, acc, tbl, rws)  = build_stuff_up_h e empty_tbl in
+  let final_unn             = List.fold rws ~init:unn ~f:(fun b r -> And(Id(r),b)) in
+  (final_unn, acc, tbl)
+and build_stuff_up_h : expr -> tbl -> bexpr * bexpr * tbl * string list = fun e s -> match e with
+| True  -> (TT, TT, s, [])
+| False -> (TT, TT, s, [])
+| _     -> failwith "TODO"
 
-type tblmap = (string, tbl, String.comparator_witness) Map.t
 
-let rec build_stuff_up : expr -> tblmap * expr = fun e ->
-  let empty_table : tblmap = Map.empty (module String) in
-  build_stuff_up_h (empty_table, e)
-and build_stuff_up_h : tblmap * expr -> tblmap * expr = fun (s, e) -> match e with
-| Bind(i, b, t) -> 
-  let x = (match b with 
-  | Flip f    -> prob_to_expect f
-  | Reward k  -> rw_to_expect k
-  | True      -> prob_to_expect 1. 
-  | False     -> prob_to_expect 0. 
-  | y         -> Expr(y) ) in
-  let s' = Map.add_exn s ~key:i ~data:x in build_stuff_up_h (s', t)
-| x             -> (s, x)
+let infer _ = failwith "TODO"
+
 
 (* let bc' : rsdd_bdd_builder -> tblmap * expr -> cf = fun bdd (s, e) -> 
 match e with
