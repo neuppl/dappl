821b99960e52eb6cc240c8e022084accd8b0d611
Minsung Cho <minsung@ccs.neu.edu>
2024-07-04 12:15:22 -0400

    make experiment generation more streamined pt1

--- experiments/mk_expr.ml
+++ experiments/mk_expr.ml
@@ -1,92 +1,59 @@
-(* This file defines utilities to generate dappl strings.
+(* This file defines utilities to generate dappl AST. *)
 
-open Random
 open Core
-open OUnit2
+open Dappl.Core_grammar
 
-(* We make variable names via a ref count. *)
-type var = Flip | Choice | Decision | Else
-type varname = string
-let ct = ref 0 ;;
+(* Produces rewards *)
+let mk_reward (n : float) (e : expr) = Reward(n, e)
 
-let mk_varname (x : var) : varname =
-  let v = Int.to_string !ct in
-  let _ : unit = ct := !ct + 1 in
-  match x with
-  | Flip      -> "f" ^ v
-  | Choice    -> "c" ^ v
-  | Decision  -> "d" ^ v
-  | Else      -> "b" ^ v
+let mk_reward_rand (e : expr) = Reward(Random.float_range (Float.neg 100.0) 100.0, e)
 
-(* Produces an expression reward k. IS LAZY FOR A REASON!!! *)
-let mk_reward (_ : unit) : string =
-  let x = Int.to_string (int 100) in
-  String.concat ~sep:" " ["reward" ; x]
-let mk_reward_det (x : int) : string =
-  String.concat ~sep:" " ["reward" ; Int.to_string x]
+let mk_reward_ret (n : float) (b : bool) =
+  if b then Reward(n, Return(True)) else Reward(n, Return(False))
 
-(* produces an expression x <- (flip f); IS LAZY FOR A REASON!!! *)
-let mk_flip (_ : unit): string * varname =
-  let v = mk_varname Flip in
-  let f = Float.to_string (Random.float 1.) in
-  (String.concat ~sep:" " [v; "<-"; "("; "flip"; f ; ");"], v)
-let mk_flip_det (f : float): string * varname =
-  let v = mk_varname Flip in
-  (String.concat ~sep:" " [v; "<-"; "("; "flip"; Float.to_string f ; ");"], v)
-let mk_flip_print (_ : unit): unit =
-  let (s,_) = mk_flip () in
-  Printf.printf "%s\n" s
+let mk_reward_ret_rand (b : bool) =
+  let f = Random.float_range (Float.neg 100.0) 100.0 in
+  if b then Reward(f, Return(True)) else Reward(f, Return(False))
 
-(* produces an expression x <- [c1, ... cn]; *)
-let mk_dec (i : int) : string * varname * varname list =
-  let l = List.init i ~f:(fun _ -> mk_varname Choice) in
-  let d = mk_varname Decision in
-  let str_of_choices = String.concat ~sep:", " l in
-  (String.concat ~sep:" " [d; "<-"; "["; str_of_choices; "];"], d, l)
+(* Produces flips and discretes *)
+let mk_flip (f : float) = Flip f
 
-let mk_dec_print (i : int) : unit =
-  let (s, _, _) = mk_dec i in
-  Printf.printf "%s\n" s
+let mk_flip_rand (_ : unit) = Flip (Random.float_range (Float.neg 100.0) 100.0)
+
+let mk_discrete (l :(string * float) list) = Discrete l
+
+let mk_discrete_rand (l : string list) =
+  let fl  = List.init (List.length l) ~f:(fun _ -> Random.float 1.) in
+  let sum = List.fold fl ~init:0. ~f:Float.add in
+  let fl' = List.map fl ~f:(fun x -> x /. sum) in
+  mk_discrete(List.zip_exn l fl')
+
+(* Produces decisions *)
+let mk_dec (l : string list) = Decision(l)
 
 (* Produces a choosewith *)
-let mk_choosewith (d : varname) (l : varname list) (e : string list) : string =
-  let _ : unit = assert_bool "dude. length mismatch in choosewith."
-          (List.length l = List.length e) in
-  let s = String.concat ~sep:" " ["choose" ; d ; "with"] in
-  let fn = fun (a,b) -> String.concat ~sep:" " ["|" ; a ; "->" ; "("; b; ")"] in
-  let l2 = List.map (List.zip_exn l e) ~f:fn in
-  let l = List.cons s l2 in
-  String.concat ~sep:"\n" l
-
-let mk_choosewith_print (d : varname) (l : varname list) (e : string list) =
-  let s = mk_choosewith d l e in
-  Printf.printf "%s\n" s
+let mk_choosewith (s : string) (l : string list) (b : expr list) =
+  ChooseWith(Ident s, List.zip_exn l b)
 
 (* Produces an ite *)
-
-let mk_ite (f : varname) (e : string list) : string =
-  let _ : unit = assert_bool "dude, have two conditions for an ite. cmon that's CS0 shit"
-          (List.length e = 2) in
-  let parenthesize = fun s -> String.concat ~sep:" " ["(" ; s ; ")"] in
-  let l' = List.map e ~f:parenthesize in
-  String.concat ~sep:" " ["if ("; f; ") then"; List.nth_exn l' 0; "else"; List.nth_exn l' 1]
-
-let mk_ite_print (f : varname) (e : string list) =
-  Printf.printf "%s\n" (mk_ite f e)
+let mk_ite (s : string) (t : expr) (e : expr) =
+  Ite(Ident s, t, e)
 
 (* Produces an observe *)
-let mk_observe (event : varname) : string =
-  String.concat ~sep:" " ["observe(" ; event ; ");"]
-
-let mk_observe_print (event : varname) =
-  Printf.printf "%s\n" (mk_observe event)
+let mk_observe (b : expr) (e : expr) = Observe(b,e)
 
 (* Produces an expression x <- expr *)
-let mk_bind (e : string) : (string * varname) =
-  let x = mk_varname Else in
-  let expr = String.concat ~sep:" " [x ; "<-" ;"("; e ; ");"] in
-  (expr, x)
+let mk_bind (s : string) (e : expr) (e' : expr) = Bind(s, e, e')
 
-let mk_bind_custom (id : string) (e : string) : (string * varname) =
-  let expr = String.concat ~sep:" " [id ; "<-" ;"("; e ; ");"] in
-  (expr, id) *)
+let mk_bind_dec (s : string) (l : string list) (e' : expr) = mk_bind s (mk_dec l) e'
+
+let mk_bind_disc (s : string) (l : (string * float) list) (e' : expr) = mk_bind s (mk_discrete l) e'
+
+let mk_bind_disc_rand (s : string) (l : string list) (e' : expr) = mk_bind s (mk_discrete_rand l) e'
+
+let mk_bind_flip (s : string) (f : float) (e' : expr) = mk_bind s (mk_flip f) e'
+
+let mk_bind_flip_rand (s : string) (_ : unit) (e' : expr) = mk_bind s (mk_flip_rand ()) e'
+
+(* Produces loops *)
+let mk_loop (n : int) (e : expr) = Loop(n, e)
